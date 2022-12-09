f5ca4fdd395d44bb7ac5564bcbcef6c910fadc01
EduNgaira <ngairaedwin@gmail.com>
2022-12-09 05:28:41 -0800

    4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,8 +1,12 @@
 #include<stdio.h>
 
-int main ()
+/**
+ * main - Entry
+ * 
+ * Return: Always 0
+ * /
+int main (void)
 {
-	puts("Programming is like building a multilingual puzzle)
-
-return 0;
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
 }
