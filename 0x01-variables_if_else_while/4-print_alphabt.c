596f6a87a5f1213101b4ffc60b7df914ad031b36
EduNgaira <ngairaedwin@gmail.com>
2022-12-09 14:54:26 -0800

    4-print_alphabt.c

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+/**
+ * main - Lowercase Alphabet
+ *
+ * Return 0;
+ */
+
+int main(void)
+{
+	char x = 'a';
+
+	while (x <= 'z')
+	{
+		putchar(x);
+		x++;
+	}
+	putchar('\n');
+	return (0);
+}
