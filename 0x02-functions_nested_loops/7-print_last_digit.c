9259beafc3b7b0db0b9a679c40248a3015eb7a08
EduNgaira <ngairaedwin@gmail.com>
2022-12-14 17:37:23 -0800

    8-24_hours.c

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ * print_last_digit -> prints the last digit
+ *
+ * @n: the passed args
+ *
+ * Return: last digit
+ */
+
+int print_last_digit(int);
+{
+	int x;
+
+	if (n < 0)
+	n = -n;
+	x = n % 10;
+	_putchar(x = '0');
+	return (x);
+}
---
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -0,0 +1,23 @@
+#include "main.h"
+/**
+ * jack-bauer - Prints minutes of day for Jack Bauer from 00:00 to 23:9
+ */
+
+void jack_bauer(void);
+{
+	int hour;
+	int minute;
+
+	for (hour = 0; hour <= 23; hour++)
+	{
+		for (minute = 0; minute <= 59; minute++)
+		{
+			_putchar((hour / 10) + '0');
+			_putchar((hour % 10) + '0');
+			_putchar(':');
+			_putchar((minute / 10) + '0');
+			_putchar((minute % 10) + '0');
+			_putchar('\n');
+		}
+	}
+}
