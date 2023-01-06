1cde97d77b39ecb95388a464d35a40bc62fb105e
EduNgaira <ngairaedwin@gmail.com>
2023-01-06 01:03:37 -0800

    main.h changes

--- 0x08-recursion/main.h
+++ 0x08-recursion/main.h
@@ -10,5 +10,12 @@ int _sqrt_recursion(int n);
 int is_prime_number(int n);
 int is_palindrome(char *s);
 int wildcmp(char *s1, char *s2);
+int find_strlen(char *s);
+int check_palindrome(char *s, int len, int index);
+int is_divisible(int num, int div);
+int strlen_no_wilds(char *str);
+void iterate_wild(char **wildstr);
+char *postfix_match(char *str, char *postfix);
+int wildcmp(char *s1, char *s2);
 
 #endif
