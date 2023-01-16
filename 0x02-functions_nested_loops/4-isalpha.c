71c9170d820339318855d219b469f28be7964aac
EduNgaira <ngairaedwin@gmail.com>
2023-01-16 11:26:55 -0800

    Letter

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -1,12 +1,18 @@
+#include <stdio.h>
+#include <ctype.h>
 #include "main.h"
+
 /**
- * _isalpha - check for alphabetic letters
- *
- * @c: character to check
- *
- * Return: 0 or 1 depending on condition
- */
-int _isalpha(int c);
+  * _isalpha - function to determine if variable is a letter
+  * @c: an integer being tested
+  *
+  * Return: lettertrue, 0 if not present.
+  */
+int _isalpha(int c)
 {
-	return ((c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z'));
+	if (c >= 'a' && c <= 'z' || c >= 'A' && c <= 'Z')
+		return (1);
+	else
+		return (0);
 }
+
