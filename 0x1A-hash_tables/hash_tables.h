807232a272f86d2cf4b93d487a01640ad78866c2
EduNgaira <ngairaedwin@gmail.com>
2023-04-07 02:56:00 -0700

    Creating hash table header file

--- 0x1A-hash_tables/hash_tables.h
+++ 0x1A-hash_tables/hash_tables.h
@@ -16,8 +16,7 @@ typedef struct hash_node_s
 	char *key;
 	char *value;
 	struct hash_node_s *next;
-}
-hash_node_t;
+} hash_node_t;
 
 /**
  * struct hash_table_s - Data structure of hash table
@@ -30,8 +29,7 @@ typedef struct hash_table_s
 {
 	unsigned long int size;
 	hash_node_t **array;
-}
-hash_table_t;
+} hash_table_t;
 
 hash_table_t *hash_table_create(unsigned long int size);
 unsigned long int hash_djb2(const unsigned char *str);
@@ -56,8 +54,7 @@ typedef struct shash_node_s
 	struct shash_node_s *next;
 	struct shash_node_s *sprev;
 	struct shash_node_s *snext;
-}
-shash_node_t;
+} shash_node_t;
 
 /**
  * struct shash_table_s - Hash table data structure that is sorted
@@ -74,8 +71,7 @@ typedef struct shash_table_s
 	shash_node_t **array;
 	shash_node_t *shead;
 	shash_node_t *stail;
-}
-shash_table_t;
+} shash_table_t;
 
 shash_table_t *shash_table_create(unsigned long int size);
 int shash_table_set(shash_table_t *ht, const char *key, const char *value);
